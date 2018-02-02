8cbb7d9c06bc927d925b8373478493dabed36cfd
Krishnan Pontes <krishnanpontes@gmail.com>
2018-02-02 17:50:17 -0200

    Added GET todos and tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First todo'
+}, {
+  text: 'Second todo'
+}];
+
 //clean the database before each it test
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,8 +32,8 @@ describe('POST /todos', () => {
       if (err) {
         return done(err);
       }
-      //if response ok, test the state of the database 
-      Todo.find().then((todos) => {
+      //if response ok, test the state of the database
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -42,9 +50,21 @@ describe('POST /todos', () => {
         return done(err);
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
+  });
+});
+
+describe('GET /todos', () => {
+  it ('Should fetch all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done());
   });
 });
