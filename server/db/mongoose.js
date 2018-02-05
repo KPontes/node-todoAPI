6128ed48241f9dc8b42cb10a9665b65e034eef5c
Krishnan Pontes <krishnanpontes@gmail.com>
2018-02-05 19:44:46 -0200

    preparing for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
+const REMOTE_MONGO = 'mongodb://ponteskl:dev12345@ds125628.mlab.com:25628/ponteskl-todoapi';
+const LOCAL_MONGO = 'mongodb://localhost:27017/TodoMgs';
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
 mongoose.Promise = global.Promise;
-const dbName = 'TodoMgs';
-mongoose.connect('mongodb://localhost:27017/' + dbName);
+
+mongoose.connect(MONGO_URI);
 
 module.exports = {mongoose};
