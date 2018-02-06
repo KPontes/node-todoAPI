7b3df7c7b25c587eac1a90c8026b29f1ec2757b3
Krishnan Pontes <krishnanpontes@gmail.com>
2018-02-06 00:03:37 -0200

    heroku adjust

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const REMOTE_MONGO = 'mongodb://ponteskl:dev12345@ds125628.mlab.com:25628/ponteskl-todoapi';
+const REMOTE_MONGO = 'mongodb://dev:1234@ds125628.mlab.com:25628/ponteskl-todoapi';
 const LOCAL_MONGO = 'mongodb://localhost:27017/TodoMgs';
 const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
 
